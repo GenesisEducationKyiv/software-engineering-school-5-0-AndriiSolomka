f58a416c2981abc8630eeeab0c331a5652e921f8
andrii_solomka <andriissolomka@gmail.com>
2025-05-15 11:26:57 +0300

    Refactor error handling in FetchService

--- src/fetch/fetch.service.ts
+++ src/fetch/fetch.service.ts
@@ -1,18 +1,17 @@
-import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import { HttpException, Injectable } from '@nestjs/common';
 import { AppLoggerService } from 'src/logger/app-logger.service';
 
 @Injectable()
 export class FetchService {
   constructor(private readonly logger: AppLoggerService) {}
+
   async get<T>(url: string): Promise<T> {
     const response = await fetch(url);
 
     if (!response.ok) {
-      this.logger.error('Fetch Error', response);
-      throw new HttpException(
-        `Failed to fetch data: ${response.statusText}`,
-        HttpStatus.BAD_GATEWAY,
-      );
+      const errorMessage = `Failed to fetch data: ${response.status}, ${response.statusText}`;
+      this.logger.error(`${url}, ${errorMessage}`);
+      throw new HttpException(errorMessage, response.status);
     }
 
     return (await response.json()) as T;
