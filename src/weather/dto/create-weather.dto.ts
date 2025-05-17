88fc216817b13aeb261e9e728145db5c219a5c1d
andrii_solomka <andriissolomka@gmail.com>
2025-05-17 17:06:51 +0300

    Add DTO documentation for subscription and weather queries

--- src/weather/dto/create-weather.dto.ts
+++ src/weather/dto/create-weather.dto.ts
@@ -2,9 +2,11 @@ import { IsNumber, IsString, IsNotEmpty } from 'class-validator';
 
 export class CreateWeatherDto {
   @IsNumber()
+  @IsNotEmpty()
   temperature: number;
 
   @IsNumber()
+  @IsNotEmpty()
   humidity: number;
 
   @IsString()
