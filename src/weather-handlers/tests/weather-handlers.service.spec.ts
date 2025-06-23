8aef383b6def128b88c09d59eb3434f2581c0432
andrii_solomka <andriissolomka@gmail.com>
2025-06-23 17:31:29 +0300

    refactor: simplify mock API response

--- src/weather-handlers/tests/weather-handlers.service.spec.ts
+++ src/weather-handlers/tests/weather-handlers.service.spec.ts
@@ -58,60 +58,13 @@ describe('WeatherHandlersService', () => {
     const city = 'Lviv';
     cacheMock.get.mockResolvedValueOnce(null);
 
-    const apiResponse = {
-      location: {
-        name: 'Lviv',
-        region: 'Lvivska',
-        country: 'UA',
-        lat: 49.84,
-        lon: 24.03,
-        tz_id: 'Europe/Kiev',
-        localtime_epoch: 0,
-        localtime: '',
-      },
+    clientMock.getCityWeather.mockResolvedValueOnce({
       current: {
-        last_updated_epoch: 0,
-        last_updated: '',
         temp_c: 15,
-        temp_f: 59,
-        is_day: 1,
-        condition: { text: 'Cloudy', icon: '', code: 0 },
-        wind_mph: 0,
-        wind_kph: 0,
-        wind_degree: 0,
-        wind_dir: '',
-        pressure_mb: 0,
-        pressure_in: 0,
-        precip_mm: 0,
-        precip_in: 0,
         humidity: 60,
-        cloud: 0,
-        feelslike_c: 0,
-        feelslike_f: 0,
-        windchill_c: 0,
-        windchill_f: 0,
-        heatindex_c: 0,
-        heatindex_f: 0,
-        dewpoint_c: 0,
-        dewpoint_f: 0,
-        vis_km: 0,
-        vis_miles: 0,
-        uv: 0,
-        gust_mph: 0,
-        gust_kph: 0,
-        air_quality: {
-          co: 0,
-          no2: 0,
-          o3: 0,
-          so2: 0,
-          pm2_5: 0,
-          pm10: 0,
-          'us-epa-index': 0,
-          'gb-defra-index': 0,
-        },
+        condition: { text: 'Cloudy' },
       },
-    };
-    clientMock.getCityWeather.mockResolvedValueOnce(apiResponse);
+    } as WeatherApiResponse);
 
     const expected = {
       temperature: 15,
