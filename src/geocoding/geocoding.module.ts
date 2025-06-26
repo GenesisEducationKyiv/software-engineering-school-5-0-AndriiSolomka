59032066c34f909f398926c7210c81ad7f20fe2e
andrii_solomka <andriissolomka@gmail.com>
2025-06-26 14:01:15 +0300

    refactor: remove GeocodingServiceFactory

--- src/geocoding/geocoding.module.ts
+++ src/geocoding/geocoding.module.ts
@@ -1,18 +1,26 @@
 import { Module } from '@nestjs/common';
 import { FetchModule } from 'src/fetch/fetch.module';
 import { CacheCityModule } from 'src/cache-city/cache-city.module';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { GeocodingService } from './geocoding.service';
-import { GeocodingServiceFactory } from './geocoding-factory.service';
+import { GeocodingProxyService } from 'src/proxy/geocoding/geocoding-proxy.service';
+import { FetchService } from 'src/fetch/fetch.service';
+import { CacheCityService } from 'src/cache-city/cache-city.service';
 
 @Module({
   imports: [FetchModule, CacheCityModule, ConfigModule],
   providers: [
-    GeocodingServiceFactory,
     {
       provide: GeocodingService,
-      useFactory: (factory: GeocodingServiceFactory) => factory.create(),
-      inject: [GeocodingServiceFactory],
+      useFactory: (
+        fetchService: FetchService,
+        configService: ConfigService,
+        cacheCityService: CacheCityService,
+      ) => {
+        const geocoding = new GeocodingService(fetchService, configService);
+        return new GeocodingProxyService(geocoding, cacheCityService);
+      },
+      inject: [FetchService, ConfigService, CacheCityService],
     },
   ],
   exports: [GeocodingService],
