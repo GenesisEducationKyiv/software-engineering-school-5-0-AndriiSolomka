164fa8d218915ecf16183604aed990415f40eaa3
Solomka Andrii <andriissolomka@gmail.com>
2025-07-15 00:23:58 +0300

    refactor: remove notification microservice code and configurations

--- apps/notification/src/core/notification.interface.ts
+++
@@ -1,7 +0,0 @@
-import { Frequency } from './subscription.interface';
-
-export const NotificationToken = Symbol('NotificationToken');
-
-export interface NotificationInterface {
-  sendWeatherUpdates(frequency: Frequency): Promise<void>;
-}
