dac4709e20dc43f226f2eb65e02528f4a53451e3
Solomka Andrii <andriissolomka@gmail.com>
2025-07-15 00:16:58 +0300

    feat: add database and subscription service to docker-compose files

--- scripts/run-tests.sh
+++ scripts/run-tests.sh
@@ -1,2 +1,4 @@
 #!/bin/sh
+npx prisma generate --schema ./apps/subscription/prisma/schema.prisma
+npx prisma migrate deploy --schema ./apps/subscription/prisma/schema.prisma
 npm run test:integration
