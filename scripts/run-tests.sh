ef87c63a39db18892ff8a305cf7695c2efe90d57
andrii_solomka <andriissolomka@gmail.com>
2025-07-14 23:06:03 +0300

    fix(tests): update Docker Compose command to run in detached mode and remove unnecessary npm ci step

--- scripts/run-tests.sh
+++ scripts/run-tests.sh
@@ -1,7 +1,4 @@
 #!/bin/sh
-
-npm ci
-
 npx prisma generate --schema ./apps/subscription/prisma/schema.prisma
 npx prisma migrate deploy --schema ./apps/subscription/prisma/schema.prisma
 npm run test:integration
