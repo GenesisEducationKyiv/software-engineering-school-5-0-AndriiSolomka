04622338a4d1b1ea431dadd547b40c2c9cc6c35e
Solomka Andrii <andriissolomka@gmail.com>
2025-07-15 00:24:20 +0300

    feat: implement email microservice

---
+++ apps/email/src/app.module.ts
@@ -0,0 +1,25 @@
+import { ConfigifyModule } from '@itgorillaz/configify';
+import { Module } from '@nestjs/common';
+import { EmailTransportToken } from 'apps/email/src/core/email-transport.interface';
+import { EmailToken } from 'apps/email/src/core/email.interface';
+import { EmailService } from 'apps/email/src/infrastructure/services/email.service';
+import { HttpClientModule } from 'libs/infrastructure/http/http-client.module';
+
+import { NodemailerService } from './infrastructure/providers/nodemailer.provider';
+import { EmailController } from './interface/email.controller';
+
+@Module({
+  imports: [ConfigifyModule.forRootAsync({}), HttpClientModule],
+  controllers: [EmailController],
+  providers: [
+    {
+      provide: EmailTransportToken,
+      useClass: NodemailerService,
+    },
+    {
+      provide: EmailToken,
+      useClass: EmailService,
+    },
+  ],
+})
+export class AppModule {}
---
+++ apps/email/src/main.ts
@@ -0,0 +1,33 @@
+import { NestFactory } from '@nestjs/core';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
+import { ensureLogDirExists } from 'libs/utils/logger/logger.config';
+
+import { AppModule } from './app.module';
+import { AppConfig } from '../config/app.config';
+
+async function bootstrap() {
+  ensureLogDirExists();
+
+  const appContext = await NestFactory.createApplicationContext(AppModule);
+  const config = appContext.get(AppConfig);
+
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+    AppModule,
+    {
+      transport: Transport.GRPC,
+      options: {
+        package: 'email',
+        protoPath: 'libs/proto/email.proto',
+        url: `0.0.0.0:${config.port}`,
+      },
+    },
+  );
+
+  await app.listen();
+  console.log(`Email microservice is running on gRPC port ${config.port}`);
+}
+
+bootstrap().catch((error) => {
+  console.error('Error Email app initialization:', error);
+  process.exit(1);
+});
