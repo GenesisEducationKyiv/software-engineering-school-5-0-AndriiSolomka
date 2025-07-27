4a7a399164a8646aa0c3e3c2f90392bc41c38dc3
andrii_solomka <andriissolomka@gmail.com>
2025-07-27 21:54:14 +0300

    feat(tests): rename architecture and unit tests for notification service

--- libs/infrastructure/cache/decorators/cache-logger.decorator.ts
+++ libs/infrastructure/cache/decorators/cache-logger.decorator.ts
@@ -1,14 +1,9 @@
-import { Inject } from '@nestjs/common';
 import { CacheInterface } from 'libs/core/cache/cache.interface';
-import {
-  LoggerInterface,
-  LoggerToken,
-} from 'libs/core/logger/logger.interface';
+import { LoggerInterface } from 'libs/core/logger/logger.interface';
 
 export class LoggingCacheDecorator<T> implements CacheInterface<T> {
   constructor(
     private readonly wrapped: CacheInterface<T>,
-    @Inject(LoggerToken)
     private readonly logger: LoggerInterface,
     private readonly context: string,
   ) {}
