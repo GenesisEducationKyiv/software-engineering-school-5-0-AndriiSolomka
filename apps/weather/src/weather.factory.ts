3ac44d69f2ba0e9136ad4c6d521d59a992c9ee49
andrii_solomka <andriissolomka@gmail.com>
2025-08-04 20:33:43 +0300

    refactor(logging): remove LoggingWeatherProviderDecorator

--- apps/weather/src/weather.factory.ts
+++ apps/weather/src/weather.factory.ts
@@ -5,7 +5,6 @@ import {
 } from 'libs/core/logger/logger.interface';
 
 import { CacheWeatherService } from './infrastructure/cache/cache-weather.service';
-import { LoggingWeatherProviderDecorator } from './infrastructure/decorators/providers-logging.decorator';
 import { OpenMeteoProviderService } from './infrastructure/providers/open-meteo.provider';
 import { WeatherApiProviderService } from './infrastructure/providers/weather-api.provider';
 import { WeatherProviderChain } from './infrastructure/providers/weather.provider';
@@ -22,18 +21,10 @@ export class WeatherFactory {
   ) {}
 
   create() {
-    const loggedWeatherApi = new LoggingWeatherProviderDecorator(
+    const chain = new WeatherProviderChain(this.logger, [
       this.apiProvider,
-      this.logger,
-      'WeatherApiProviderService',
-    );
-    const loggedMeteo = new LoggingWeatherProviderDecorator(
       this.openMeteo,
-      this.logger,
-      'OpenMeteoProviderService',
-    );
-
-    const chain = new WeatherProviderChain([loggedWeatherApi, loggedMeteo]);
+    ]);
     return new WeatherCacheProxyService(chain, this.cache);
   }
 }
